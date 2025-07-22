00e3f68b5771ad4885cf69eb893a90c46737f6e7
Tomas Weiss <tomas.weiss2@gmail.com>
2025-07-22 14:04:31 +0200

    chore: missing licence
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>
    Signed-off-by: Aleš Kalfas <kalfas.ales@gmail.com>

--- agents/official/beeai-framework/chat/src/chat/tools/react/clarification.py
+++ agents/official/beeai-framework/chat/src/chat/tools/react/clarification.py
@@ -1,3 +1,6 @@
+# Copyright 2025 © BeeAI a Series of LF Projects, LLC
+# SPDX-License-Identifier: Apache-2.0
+
 from pydantic import BaseModel, Field
 from beeai_framework.tools import Tool, ToolRunOptions, StringToolOutput
 from beeai_framework.context import RunContext
